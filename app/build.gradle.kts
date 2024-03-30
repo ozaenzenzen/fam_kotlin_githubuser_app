76bb1be5c928aace0404347c15d9cfa3eb4435e9
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-03-30 11:50:52 +0700

    up sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.famgithubuser"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.famgithubuser"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

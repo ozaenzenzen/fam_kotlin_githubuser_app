ecb37359e0dbeec18d4de6f4ada1f164c87b24e4
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-05-17 17:12:39 +0700

    update for home xml

--- app/src/main/java/com/example/famgithubuser/MainActivity.kt
+++ app/src/main/java/com/example/famgithubuser/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.famgithubuser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.famgithubuser.databinding.ActivityMainBinding
+import com.google.android.material.appbar.AppBarLayout
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +17,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+// Set up the toolbar
+        // Set up the toolbar
+//        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar))
+
+        // Ensure SearchView is expanded by default
+        val searchView = findViewById<SearchView>(R.id.search_view)
+        if (searchView != null) {
+            searchView.setIconifiedByDefault(false)
+        }
 
 //        setSupportActionBar(binding.appBarMain.toolbar)
 
